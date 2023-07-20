37fccb720f80789bf139098bc710cc1b5d9d3db8
LongJourney47 <fernandezjw07@gmail.com>
2023-07-20 14:51:20 -0400

    The code has basic css module styling. Along side, a functioning chat AI system that uses module chatgpt 3.5. Moreover, a skeleton for a chat history array

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,154 @@
-import Image from 'next/image'
+"use client"; // This is a client component
 
-export default function Home() {
+// move chat array into the client side
+// use usestate on top of it
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { NextPage } from "next";
+import Head from "next/head";
+import homePage from "@/app/homePage.module.css";
+// import { chatArr } from "@/app/api/chatgpt/route";
+// import './globals.module.css'
+
+const Home: NextPage = (chatArr) => {
+
+  const [textareaValue, setTextareaValue] = useState("");
+  const [resultArray, setResultArray] = useState<string[]>([]);
+  const [chatData, setChatData] = useState([]);
+  
+  const [messages, setMessages] = useState([
+    { type: "system", content: "however i’m prompt tuning model" },
+    { type: "user", content: "Whatever the user types in as their prompt?" },
+  ]);
+
+   // Function to save chat data to local storage
+   const saveChatDataToLocalStorage = (chatData:any) => {
+    localStorage.setItem("chatData", JSON.stringify(chatData));
+  };
+
+  // Function to load chat data from local storage on page load
+  const loadChatDataFromLocalStorage = () => {
+    const storedChatData = localStorage.getItem("chatData");
+    if (storedChatData) {
+      setChatData(JSON.parse(storedChatData));
+    }
+  };
+
+  // Load chat data from local storage during the initial render
+  useEffect(() => {
+    loadChatDataFromLocalStorage();
+  }, []);
+
+  // Save chat data to local storage whenever chatData changes
+  useEffect(() => {
+    saveChatDataToLocalStorage(chatData);
+  }, [chatData]);
+
+  const handleTextareaChange = (e: any) => {
+    setTextareaValue(e.target.value);
+  };
+
+  async function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    // setIsLoading(true);
+    const form = e.currentTarget as HTMLFormElement;
+    const userInput = form.message.value.trim();
+    if (!userInput) return;
+
+    axios
+      .post(
+        "/api/chatgpt",
+        { product: userInput },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        console.log("response - line 116", response);
+
+        // ----- add the code for later to test -----
+        const userMessage = { type: "user", content: userInput };
+
+        const assistantMessage = {
+          type: "assistant",
+          content: response.data.item, // Assuming the API response contains an 'item' property with the response content
+        };
+        // setMessages((prevMessages) => [...prevMessages, userMessage]);
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          userMessage,
+          assistantMessage,
+        ]);
+
+        let rawResult = response.data.item;
+        // Append the new result to the array
+        setResultArray((prevResults) => [...prevResults, rawResult]);
+        console.log("chat history 2!! ", resultArray);
+        form["message"].value = "";
+      })
+      .catch((error) => {
+        console.error("Error:", error.message);
+      });
+  }
+
+  console.log("chat history 2!! ", resultArray);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <main className={homePage.container}>
+        <Head>
+          {/* <title>ChatGPT Clone</title> */}
+          <meta name="description" content="ChatGPT Clone with Next.js" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <h1 className={homePage.title}>ChatGPT Clone</h1>
+        <div className="space-y-4">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`${homePage.message} ${
+                message.type === "user"
+                  ? homePage["user-message"]
+                  : homePage["assistant-message"]
+              }`}
+            >
+              {message.content}
+            </div>
+          ))}
         </div>
+
+        {/* <form onSubmit={onSubmit} className="mt-4"> */}
+
+        <form onSubmit={onSubmit} className={homePage.inputContainer}>
+          <textarea
+            name="message"
+            className={homePage.textarea}
+            onChange={handleTextareaChange}
+          />
+          <button
+            type="submit"
+            // className={homePage["send-button"]}
+            className={`${homePage["send-button"]} ${
+              !textareaValue ? homePage.hide : ""
+            }`}
+          >
+            Send
+          </button>
+        </form>
+      </main>
+
+      <div className={homePage["chat-log"]}>
+        {resultArray.map((result, index) => (
+          <div key={index} className={homePage["chat-log-item"]}>
+            {result}
+          </div>
+        ))}
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
